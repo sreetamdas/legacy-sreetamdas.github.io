6c61491267f7180702b24b0d54bbfeea8a0b9c80
Sreetam Das <sreetamdas@gmail.com>
2018-10-17 21:34:42 +0530

    production soon pls

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,6 @@
 import React from "react";
 import CustomNav from "./Navbar";
 // import Overlay from "./Overlay";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import Loves from "./Loves";
 import Footer from "./Footer";
 
@@ -13,16 +11,35 @@ class Landing extends React.Component {
 				<CustomNav />
 				<div style={{ fontSize: "40px" }}>
 					<div style={{ paddingTop: "200px" }} className="container">
-						Hey!
+						Hey! I'm Sreetam Das,
 						<br />
-						I'm Sreetam Das.
+						<span style={{ fontSize: "30px" }}>
+							a frontend developer who graduated from{" "}
+							<a
+								href="https://www.nitw.ac.in"
+								className="twitter-blue"
+							>
+								NIT Warangal
+							</a>
+							. Currently working as an Enterprise Solutions
+							Architect at{" "}
+							<a
+								href="https://www.microland.com"
+								className="twitter-blue"
+							>
+								Microland
+							</a>
+							. I love clean and minimalist interfaces and
+							component-based development. I also love{" "}
+							<a
+								href="https://qcnitw.in"
+								className="twitter-blue"
+							>
+								Quizzing
+							</a>
+							.
+						</span>
 						<br />
-						<br />I{" "}
-						<FontAwesomeIcon
-							icon={faHeart}
-							color="red"
-							size="sm"
-						/>{" "}
 						<Loves />
 					</div>
 				</div>
