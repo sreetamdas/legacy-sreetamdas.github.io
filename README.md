19d936a5f2983512bb018822528668d187a365ce
Sreetam Das <sreetamdas@gmail.com>
2018-10-17 22:30:18 +0530

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-# sreetamdas.github.io
-
-Hey there!
-I see you've taken some interest in my [website](https://sreetamdas.github.io).
-
-Feel free to contribute or fork it out for yourself!
-
-## License
-MIT (or just go ahead and do anything, really)
---
+++ precache-manifest.0bcaf02db6daf2dd871b382bd5d3347f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d580522cabcfd271f2e3a29b0237af3",
+    "url": "/static/media/dogs.3d580522.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "962d74703c51fac5b78a",
+    "url": "/static/js/main.962d7470.chunk.js"
+  },
+  {
+    "revision": "3bdeb84d8b4636c5c062",
+    "url": "/static/js/1.3bdeb84d.chunk.js"
+  },
+  {
+    "revision": "962d74703c51fac5b78a",
+    "url": "/static/css/main.a2479241.chunk.css"
+  },
+  {
+    "revision": "3bdeb84d8b4636c5c062",
+    "url": "/static/css/1.75c5773b.chunk.css"
+  },
+  {
+    "revision": "bcbab7ee8f2f62077c6bf17eb79f535f",
+    "url": "/index.html"
+  }
+];
