1deda29d692dca144db469614fbf7eeb90af194e
Sreetam Das <sreetamdas@gmail.com>
2019-09-19 16:53:43 +0530

    Updates

---
+++ precache-manifest.79aedac66ab09dd6d78c8ef621f0ffcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d580522cabcfd271f2e3a29b0237af3",
+    "url": "/static/media/dogs.3d580522.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b7dbf1f62fa056b85d0e",
+    "url": "/static/js/main.b7dbf1f6.chunk.js"
+  },
+  {
+    "revision": "8d0066efcb63bd2807d0",
+    "url": "/static/js/1.8d0066ef.chunk.js"
+  },
+  {
+    "revision": "b7dbf1f62fa056b85d0e",
+    "url": "/static/css/main.44ea24b7.chunk.css"
+  },
+  {
+    "revision": "8d0066efcb63bd2807d0",
+    "url": "/static/css/1.e62577a5.chunk.css"
+  },
+  {
+    "revision": "1c1868903189971da13c7e4983292cc5",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cede89d792d4a35cd47ebf980dbf7453.js"
+  "/precache-manifest.79aedac66ab09dd6d78c8ef621f0ffcb.js"
 );
 
 workbox.clientsClaim();
